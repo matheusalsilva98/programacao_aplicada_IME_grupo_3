930d2e4bed05b7edfb300a534e928284e6a55801
matheusalsilva98 <matheus.silva@ime.eb.br>
2023-04-03 20:50:04 -0300

    Update solucao_complementar.py

--- algorithms/Projeto1/solucao_complementar.py
+++ algorithms/Projeto1/solucao_complementar.py
@@ -150,3 +150,104 @@ class Projeto1SolucaoComplementar(QgsProcessingAlgorithm):
                     # Ajeitando a geometria com o setGeometry() para conseguir adicionar os vértices.
                     quadFeatureI.setGeometry(QgsGeometry.fromWkt(f"POLYGON (({pontoI1}, {pontoI2}, {pontoI3}, {pontoI4}, {pontoI1}))"))
                     quadFeatureJ.setGeometry(QgsGeometry.fromWkt(f"POLYGON (({pontoJ1}, {pontoJ2}, {pontoJ3}, {pontoJ4}, {pontoJ1}))"))
+
+                    # Verificando se há intersecção das duas geometrias.
+                    if (quadFeatureI.geometry()).overlaps(quadFeatureJ.geometry()):
+                        # Com o método 'intersection' resulta a geometria da interseção e armazenando na variável
+                        # chamada de quadInterseccao.
+                        quadInterseccao = (quadFeatureI.geometry()).intersection(quadFeatureJ.geometry())
+
+                        # Realizando o Bounding Box da intersecção, podemos utilizar o método boundingBox() na
+                        # Variável quadInterseccao.
+
+                        bbox = quadInterseccao.boundingBox()
+
+                        # Gerando as coordenadas das abcissas do bounding box, colocando em uma lista, em que construímos
+                        # partindo do x mínimo até o x máximo do bounding box, realizando o mesmo com o y, além disso, a quantidade de elementos que será dado é o inteiro da diferença das abcissas.
+
+                        coordX = np.linspace(bbox.xMinimum(), bbox.xMaximum(), num=int((bbox.xMaximum() - bbox.xMinimum())//200))
+
+                        # Realizando o mesmo para o y, teremos:
+
+                        coordY = np.linspace(bbox.yMinimum(), bbox.yMaximum(), num=int((bbox.yMaximum() - bbox.yMinimum())//200))
+
+                        # Criando a variável que irá ser responsável por receber as coordenadas de todos os pontos que estão no bounding box, variando o x do mínimo para o máximo além do y do mínimo para o máximo.
+
+                        tuplasCoordenadas = []
+
+                        # Para adiconar na lista de coordenadas podemos fazer dois for, para percorrer todos os valores possíveis da coordenada x, com todos os possíveis valores da coordenada y.
+
+                        for k in range(len(coordX)):
+                            for l in range(len(coordY)):
+                                tuplasCoordenadas.append(f"({coordX[k]} {coordY[l]})")
+
+                        # Iterando sobre todas as coordenadas dos pontos presentes na intersecção dos rasters.
+
+                        for coordenada in tuplasCoordenadas:
+                            
+                            ponto = QgsFeature()
+                            ponto.setGeometry(QgsGeometry.fromWkt(f"POINT {coordenada}"))
+
+                            if (ponto.geometry()).within(quadInterseccao):
+                                
+                                # Criando a feature chamada de flagFeature que irá receber esses pontos que estão na
+                                # intersecção dos pontos e do polígono, que será o responsável por pegar o componen-
+                                # te z de cada raster e irá anaalisar o ez_i, além do EMQz entre as imagens no final.
+                                flagFeature = QgsFeature(fields)
+
+                                flagFeature.setGeometry(QgsGeometry.fromWkt(quadInterseccao.asWkt()))
+
+                                # Pegaremos então para calcular o erro do z, como o primeiro raster analisado como sendo
+                                # o que temos por verdade, e o segundo raster com o z teste.
+                                z_t = listaRaster[j].dataProvider().sample(QgsPointXY(ponto.geometry().asPoint()), 1)[0]
+                                z_r = listaRaster[i].dataProvider().sample(QgsPointXY(ponto.geometry().asPoint()), 1)[0]
+
+                                # Dessa forma, vamos preencher os atributos da feição feita.
+                                flagFeature["raster1"] = "teste1"
+                                flagFeature["raster2"] = "teste2"
+                                flagFeature["erro"] = (z_t - z_r)
+
+                                # Adiciona a feature na camada de saída.
+                                sink.addFeature(flagFeature, QgsFeatureSink.FastInsert)
+
+            # Progresso da barra na interface com o usuário.
+            feedback.setProgress(int(current * total))
+
+        # Retorno do processo realizado pelo algoritmo.
+        return {self.OUTPUT: dest_id}
+
+    def name(self):
+        """
+        Retorna o nome do algoritmo traduzido.
+        """
+        return 'Solução Complementar do Projeto 1'
+
+    def displayName(self):
+        """
+        Returns the translated algorithm name, which should be used for any
+        user-visible display of the algorithm name.
+        """
+        return self.tr(self.name())
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr(self.groupId())
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'Projeto 1'
+
+    def tr(self, string):
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return Projeto1SolucaoComplementar()
